ca89542531a25c9c3ab1a1c8b2b407545680aa72
GavinReidJones <Gavin7009@hotmail.com>
2017-10-03 11:35:53 +0100

    Update UoN_Hello_World.cpp
    Added Comments

--- UoN_Hello_World.cpp
+++ UoN_Hello_World.cpp
@@ -2,12 +2,15 @@
 //
 
 #include "stdafx.h"
+// utilises iostream library to allow text input and output by the user
 #include <iostream>
+// utilises C++ Math library to allow for  more complex calculations
 #include <cmath>
 
-
+// Main Program Loop
 int main()
 {
+	// Prints Message
 	std::cout << "Hello World";
     return 0;
 }
