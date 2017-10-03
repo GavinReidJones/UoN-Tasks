823ae512699e57fc7eb0acd22ece1f83f9446e09
GavinReidJones <Gavin7009@hotmail.com>
2017-10-03 11:33:46 +0100

    Add files via upload
    Task 1

---
+++ UoN_Hello_World.cpp
@@ -0,0 +1,14 @@
+// UoN_Hello_World.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+	std::cout << "Hello World";
+    return 0;
+}
+
